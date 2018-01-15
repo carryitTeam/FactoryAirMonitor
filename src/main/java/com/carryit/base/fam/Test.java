885a30fc855428b20e35cf89d7ec176d9d72f502
zhl邹海林 <hlzou@ctrip.com>
2018-01-15 09:21:43 +0800

    add test files

---
+++ src/main/java/com/carryit/base/fam/Test.java
@@ -0,0 +1,7 @@
+package com.carryit.base.fam;
+
+/**
+ * Created by hlzou on 2018/1/15.
+ */
+public class Test {
+}
---
+++ src/test/java/com/carryit/base/fam/TestTest.java
@@ -0,0 +1,10 @@
+package com.carryit.base.fam;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by hlzou on 2018/1/15.
+ */
+public class TestTest {
+
+}
